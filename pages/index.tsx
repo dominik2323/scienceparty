b4bfb4e842ad4927ce91403c402a5304ad1a0218
Dominik <dominik.tomcik23@gmail.com>
2020-08-09 11:38:42 +0200

    Added btn for social network

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ function Index({}: Props): ReactElement {
   return (
     <div className={`view`}>
       <Img src={`/icons/heroMotiv.svg`} />
+      <a className={`fcb`} href='https://www.facebook.com/SciencePartyBrno/'>
+        Pokračovat na Facebook
+      </a>
     </div>
   );
 }
